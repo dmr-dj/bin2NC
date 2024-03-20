901daf48b5a734c14c8cafc30fbe3d8e27e83aba
dmr-dj <didier.roche@lsce.ipsl.fr>
2024-03-20 22:50:34 +0100

    Small clean up of main

--- src/main.f90
+++ src/main.f90
@@ -6,9 +6,6 @@ program main
   use global_constants_mod, only: dblp=>dp
   implicit none
 
-!~   call main_work('test-data/temsg2.dat',"temsg2.nc","temsg",.true.)
-!~   call main_work("temsg2.nc", 'temsg2-chck.dat',"temsg",.false.)
-
    type(dynamicString), allocatable :: FileList(:), SystemInfo(:)
    character(63) :: record
    character(len=127)            :: searchString, outputfile
@@ -19,7 +16,6 @@ program main
    real*16                       :: dummy
 
    searchString = 'shrsg*.dat'
-   ! searchString = 'pi_corr_tem*.dat'
    FileList = getFileList(searchString)
 
    if (size(FileList)==0) then
@@ -30,14 +26,11 @@ program main
          
          outputfile=trim(basename(FileList(i)%record, ".dat"))//extension
          write(*,*) "Converting: -> ", FileList(i)%record, " " , trim(outputfile)
-         ! call main_work(FileList(i)%record,trim(outputfile),"tem",.true.)
          call main_work(FileList(i)%record,trim(outputfile),"rcc",.true.,stdnm="relative_cloud_cover")
        end do
-!~        call main_work("temsg230.dat","temsg230.nc","temsg",.true.)
    end if
 
    searchString = 'wndsg*.dat'
-   ! searchString = 'pi_corr_prc*.dat'
    FileList = getFileList(searchString)
 
    if (size(FileList)==0) then
@@ -48,14 +41,11 @@ program main
          
          outputfile=trim(basename(FileList(i)%record, ".dat"))//extension
          write(*,*) "Converting: -> ", FileList(i)%record, " " , trim(outputfile)
-         ! call main_work(FileList(i)%record,trim(outputfile),"prc",.true.)
          call main_work(FileList(i)%record,trim(outputfile),"wnd",.true.,stdnm="wind_speed")
        end do
-!~        call main_work("temsg230.dat","temsg230.nc","temsg",.true.)
    end if
 
    searchString = 'temsg*.dat'
-   ! searchString = 'pi_corr_prc*.dat'
    FileList = getFileList(searchString)
 
    if (size(FileList)==0) then
@@ -69,12 +59,10 @@ program main
          ! call main_work(FileList(i)%record,trim(outputfile),"prc",.true.)
          call main_work(FileList(i)%record,trim(outputfile),"tas",.true.,stdnm="surface_temperature")
        end do
-!~        call main_work("temsg230.dat","temsg230.nc","temsg",.true.)
    end if
 
 
    searchString = 'prcsg*.dat'
-   ! searchString = 'pi_corr_prc*.dat'
    FileList = getFileList(searchString)
 
    if (size(FileList)==0) then
@@ -85,14 +73,11 @@ program main
          
          outputfile=trim(basename(FileList(i)%record, ".dat"))//extension
          write(*,*) "Converting: -> ", FileList(i)%record, " " , trim(outputfile)
-         ! call main_work(FileList(i)%record,trim(outputfile),"prc",.true.)
          call main_work(FileList(i)%record,trim(outputfile),"pr",.true.,stdnm="precipitation_flux",multiple=1._dblp/86400._dblp)
        end do
-!~        call main_work("temsg230.dat","temsg230.nc","temsg",.true.)
    end if
 
    searchString = 'rhusg*.dat'
-   ! searchString = 'pi_corr_prc*.dat'
    FileList = getFileList(searchString)
 
    if (size(FileList)==0) then
@@ -103,10 +88,8 @@ program main
          
          outputfile=trim(basename(FileList(i)%record, ".dat"))//extension
          write(*,*) "Converting: -> ", FileList(i)%record, " " , trim(outputfile)
-         ! call main_work(FileList(i)%record,trim(outputfile),"prc",.true.)
          call main_work(FileList(i)%record,trim(outputfile),"hur",.true.,stdnm="relative_humidity")
        end do
-!~        call main_work("temsg230.dat","temsg230.nc","temsg",.true.)
    end if
 
 end program main
